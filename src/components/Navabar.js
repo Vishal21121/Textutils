fa53afc77cf09ffcabf46b5b4e2c4fd82063940f
Vishal21121 <vs84077000@gmail.com>
2022-11-26 18:52:54 +0530

    modified navbar

--- src/components/Navabar.js
+++ src/components/Navabar.js
@@ -12,7 +12,7 @@ function Navabar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">Text Editor</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
